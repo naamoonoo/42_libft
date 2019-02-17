2240d320c2e8de5413799e676e905749c5ab44e9
Hyunwoo Nam <hnam@e1z2r6p10.42.us.org>
2019-02-17 10:41:47 -0800

    start lst

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_same.c                                       :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/17 10:40:34 by hnam              #+#    #+#             */
-/*   Updated: 2019/02/17 10:40:36 by hnam             ###   ########.fr       */
+/*   Created: 2019/02/17 10:40:56 by hnam              #+#    #+#             */
+/*   Updated: 2019/02/17 10:40:57 by hnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_is_same(char *haystack, const char *needle)
-{
-    unsigned int i;
-    unsigned int ans;
 
-    i = -1;
-    ans = 0;
-    while (needle[++i])
-        if (haystack[i] == needle[i])
-            ans++;
-    return (i == ans);
-}
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_same.c                                       :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/17 10:40:34 by hnam              #+#    #+#             */
-/*   Updated: 2019/02/17 10:40:36 by hnam             ###   ########.fr       */
+/*   Created: 2019/02/17 10:27:24 by hnam              #+#    #+#             */
+/*   Updated: 2019/02/17 10:40:45 by hnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_is_same(char *haystack, const char *needle)
-{
-    unsigned int i;
-    unsigned int ans;
 
-    i = -1;
-    ans = 0;
-    while (needle[++i])
-        if (haystack[i] == needle[i])
-            ans++;
-    return (i == ans);
-}
