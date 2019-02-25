c592b8f08b1f3123aa4c429b58f2b6a500f1791b
naamoonoo <naamoonoo.y@gmail.com>
2019-02-24 21:42:01 -0800

    .

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t size)
 {
 	char *res;
 
-	if (!(res = (char *)malloc(size * sizeof(res))))
+	if (!(res = (char *)malloc(size * sizeof(char))))
 		return (NULL);
 	ft_memset(res, 0, size);
 	return (res);
